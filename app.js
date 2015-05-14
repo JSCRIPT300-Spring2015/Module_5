c0a8412e28f1f9ce7d5909eda37ec4fce7f06c47
Dennis T Yip <t80532292@hotmail.com>
2015-05-13 22:38:34 -0700

    Restructure the 4 service to use router

--- app.js
+++ app.js
@@ -1,62 +1,16 @@
 var express = require('express');
 var app = express();
 
+var router = require('./routes/truckRoutes');
+
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/foodTruckAPI');
 
 var bodyParser = require('body-parser');
-var Truck = require('./models/truckModel');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
-
-app.get('/trucks', function(request, response) {
-    Truck.find(function(error, trucks) {
-        if (error) {
-            response.status(500).send(error);
-        } else {
-            response.json(trucks);
-        }
-    });
-});
-
-app.get('/trucks/:id', function(request, response) {
-    Truck.findById(request.params.id, function(error, truck) {
-        if (error) {
-            response.status(500).send(error);
-        } else {
-            response.json(truck);
-        }
-    });
-});
-
-app.post('/trucks', function (request, response) {
-    var newTruck = new Truck(request.body);
-
-    console.log(newTruck);
-
-    newTruck.save(function(error, savedTruck) {
-        if (error) {
-            response.status(500).send(error);
-        } else {
-            response.status(201).json(savedTruck);
-        }
-    });
-});
-
-app.delete('/trucks/:id', function (request, response) {
-    var id = request.params.id;
-
-    Truck.findByIdAndRemove(id, function(error, truck) {
-        if (error) {
-            console.log(err);
-            response.status(500).send(error);
-        } else {
-            response.sendStatus(200);
-        }
-    })
-});
+app.use('/', router);
 
 app.listen(3000, function () {
     console.log('listening on port 3000 by dennis the foodTruck');
