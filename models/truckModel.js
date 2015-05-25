051e03c91642d909326c729f2f5592c4af7408ab
MMG <gamboa_girl@hotmail.com>
2015-05-25 13:28:04 -0700

    Revert "Update truckModel.js"
    This reverts commit 6f0d280fc8760d2516de6cc638cc6c35df091be5.

--- models/truckModel.js
+++ models/truckModel.js
@@ -1,7 +1,4 @@
 // below you'll find the schema you need to export your truckModel
-// when exporting the mongoose model you will create using this schema, 
-// name your model 'Truck' (see the example in the slide deck for 'Book')
-// this is to match the name of the collection the data was imported into
 
 // var foodTruckSchema = new Schema({
 // 	name: String,
