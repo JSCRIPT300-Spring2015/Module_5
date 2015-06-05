3a0f26cfbe2afe4e7bde223d2ae325591150e2d5
MMG <gamboa_girl@hotmail.com>
2015-06-04 22:51:02 -0700

    Updates truckRoutes.js and app.js
    Also includes .git.ignore and package.json

--- models/truckModel.js
+++ models/truckModel.js
@@ -1,12 +1,16 @@
-// below you'll find the schema you need to export your truckModel
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-// var foodTruckSchema = new Schema({
-// 	name: String,
-// 	foodType: [String],
-// 	schedule: [String],
-// 	payment: [String],
-// 	description: String,
-// 	website: String,
-// 	Facebook: String,
-// 	Twitter: String
-// });
+// below you'll find the schema you need to export your truckModel
+var foodTruckSchema = new Schema({
+    name: String,
+ 	foodType: [String],
+ 	schedule: [String],
+ 	payment: [String],
+ 	description: String,
+ 	website: String,
+ 	Facebook: String,
+ 	Twitter: String
+});
+
+module.exports = mongoose.model('Truck', foodTruckSchema);
